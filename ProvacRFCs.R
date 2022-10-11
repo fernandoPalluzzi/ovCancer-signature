a2a2eef429f91a363dc4546430016d769806af36
Fernando Palluzzi <51379764+fernandoPalluzzi@users.noreply.github.com>
2022-10-11 14:41:25 +0200

    Update ProvacRFCs.R

--- ProvacRFCs.R
+++ ProvacRFCs.R
@@ -1,3 +1,26 @@
+# ovCancer-signature - Random Forest Classifier module
+
+#  Copyright (C) 2022 Fernando Palluzzi
+#  e-mail: <fernando.palluzzi@gmail.com>
+#  Bioinformatics facility 
+#  Gemelli Science and Technological Park (GSTeP)
+#  Fondazione Policlinico Universitario Agostino Gemelli IRCCS,
+#  Largo Agostino Gemelli 8, 00168 Roma, Italy
+
+#  ovCancer-signature is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  ovCancer-signature is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ##### Import required libraries
 
 library(randomForest)
